1aafb69b7d3fbd0a7c781cc69c7b0cda8b093917
AmitKapilaCodeIns <amitkapila@hotmail.com>
2025-06-09 17:02:17 +0100

    change view

--- about/views.py
+++ about/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def about_me(request):
+    return HttpResponse("This would be the about page")
+    return HttpResponse(
+        request.method + " " + request.path + " " + str(request.META)
+    )
 
 # Create your views here.
