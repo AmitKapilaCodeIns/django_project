880939ed5dfff8d9b54e82f4746eb83ce66cd1f9
AmitKapilaCodeIns <amitkapila@hotmail.com>
2025-06-11 20:30:24 +0100

    Disabled debug

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-pb5f!t&+m5$owqkpt=ty49i&bk_95e$drb!wnyd!oridxx)ann'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1',]
 
